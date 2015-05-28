dfac36ec50fc4cb30f86eae619293e39e66cee1d
jassisidhu <jassi.kaur667@gmail.com>
2015-05-27 20:42:42 -0400

    Added story slides

---
+++ FindTheBody/scripts/game.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="typings/impress/impress.d.ts" />
+
+//function to display some data on console 
+function init(){
+    console.log("Game is running");
+    impress().init();
+}
